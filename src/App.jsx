6a333f2e8e13f588c53340ec9831d7195b617cf3
jasonwj96 <jasonwj96@gmail.com>
2019-08-04 08:34:09 -0500

    Added navbar, fonts

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="App" id="dark">
+      <p>app works</p>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div>
+      <p>Navbar works</p>
+    </div>
+  );
+};
+
+export default Navbar;
