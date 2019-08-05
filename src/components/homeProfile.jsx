6f42c76f26abecc5b81724b2fe195f695f51ab5c
jasonwj96 <jasonwj96@gmail.com>
2019-08-05 15:44:40 -0500

    Visual tweaks

--- src/components/homeProfile.jsx
+++ src/components/homeProfile.jsx
@@ -9,7 +9,11 @@ const HomeProfile = () => {
         <h1>Jason Wedderburn</h1>
         <h3>Fullstack developer</h3>
         <div className="separator" />
-        <p>My name is Jason Alejandro Wedderburn Johnson, I'm 22 years old</p>
+        <p>
+          My name is Jason Alejandro Wedderburn Johnson, I'm 22 years old. I was
+          born on August 28th, 1996, in Panama City, Panama. Currently I'm a
+          student at the Technologial University of Panama.
+        </p>
       </div>
       <div id="profile-img">
         <img src={profilebg} alt="Profile" />
